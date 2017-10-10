e702df02d4760b2bff7c9433fdc795146d976fb1
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-11 00:41:17 +0300

    Update Main.java

--- ex12d/src/ru/itis/Main.java
+++ ex12d/src/ru/itis/Main.java
@@ -9,9 +9,9 @@ public class Main {
         int pow=-1;
         int p = 1;
         double s=0,eps=0.0001,t=1;
-        while(Math.abs(s)>eps){
+        while(Math.abs(s)>eps){  //логика неверная
             p*=x*x;
-            t*=1.0*pow*p/2*n;
+            t*=1.0*pow*p/2*n;  //формула неверная
             n++;
             pow=-pow;
             s+=t;
