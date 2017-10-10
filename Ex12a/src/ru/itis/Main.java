3750ca7851cc0b3a8000b92d435778be64740c9e
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-10 21:44:06 +0300

    New line

--- Ex12a/src/ru/itis/Main.java
+++ Ex12a/src/ru/itis/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
-	int x=in.nextInt();
+	double x=in.nextInt();
 	int n=1;
 	double eps=0.0001,s=0,t=1;
 	while(Math.abs(t)>eps){
