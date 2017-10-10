9b28b2f15e2e0cdcb7a849b2b5b883e4d04203dc
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-11 00:38:10 +0300

    Update Main.java

--- Ex12b/src/ru/itis/Main.java
+++ Ex12b/src/ru/itis/Main.java
@@ -8,8 +8,8 @@ public class Main {
         int n=1;
         int pow=1;
         double s=x,eps=0.0001,t=1;
-        while(Math.abs(s)>eps){
-            t*=1.0*pow*x*x/(2*n+1);
+        while(Math.abs(s)>eps){     //почему оценивается сумма?
+            t*=1.0*pow*x*x/(2*n+1);   //неверная формула
             n++;
             pow=-pow;
             s+=t;
