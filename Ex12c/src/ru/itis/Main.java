09002dedb16c989315ff5b680c3f076a279046eb
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-10 23:25:21 +0300

    ex12c

---
+++ Ex12c/src/ru/itis/Main.java
@@ -0,0 +1,20 @@
+package ru.itis;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        double x=in.nextInt();
+        int n=1;
+        int pow=1;
+        double eps=0.0001,s=0,t=1;
+        while(Math.abs(t)>eps){
+            t*=1.0*pow*x;
+            pow=-pow;
+            s+=t/n;
+            n++;
+        }
+        System.out.print(s);
+    }
+}
+
