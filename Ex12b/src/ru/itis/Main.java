8735b9a18044a702c56ebc8985395ffb010aa98d
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-11 15:38:02 +0300

    исправленная 11.10.17 12б

--- Ex12b/src/ru/itis/Main.java
+++ Ex12b/src/ru/itis/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
-        double x=in.nextInt();
+        double x=in.nextDouble();
         int n=1;
-        int pow=1;
-        double s=x,eps=0.0001,t=1;
-        while(Math.abs(s)>eps){
-            t*=1.0*pow*x*x/(2*n+1);
+        int pow=-1;
+        double s=0,eps=0.0001,t=1;
+        while(Math.abs(t)>eps){
+            s+=t;
+            t*=1.0*pow*x*x/(2*n)*(2*n+1);
             n++;
             pow=-pow;
-            s+=t;
         }
         System.out.print(s);
     }
