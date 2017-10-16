97d4bd6f31dfd94e40f278f16ca0a93c1c7179f0
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-16 22:33:27 +0300

    исправленная 12г 16.10

--- ex12d/src/ru/itis/Main.java
+++ ex12d/src/ru/itis/Main.java
@@ -7,11 +7,10 @@ public class Main {
         double x=in.nextInt();
         int n=1;
         int pow=-1;
-        int p = 1;
-        double s=0,eps=0.0001,t=1;
-        while(Math.abs(s)>eps){  //логика неверная
-            p*=x*x;
-            t*=1.0*pow*p/2*n;  //формула неверная
+        double eps=0.0001,t=(x*x)/2,s;
+        s=1-t;
+        while(Math.abs(t)>eps){
+            t*=((1.0*pow*x*x)/(2*n));
             n++;
             pow=-pow;
             s+=t;
