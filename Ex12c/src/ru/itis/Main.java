0068fd38413004a4f694b4c69a466e7c31cdd895
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-16 22:41:14 +0300

    ex 12в исправленная 16.10

--- Ex12c/src/ru/itis/Main.java
+++ Ex12c/src/ru/itis/Main.java
@@ -1,17 +1,21 @@
 package ru.itis;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        double x=in.nextInt();
-        int n=1;
-        int pow=1;
-        double eps=0.0001,s=0,t=1;
-        while(Math.abs(t)>eps){
-            t*=1.0*pow*x;  // договаривались не копить степень в отдельной переменной
-            pow=-pow;
-            s+=t/n;
+        Scanner in = new Scanner(System.in);
+        do {
+            double x = in.nextDouble();
+        } while (Math.abs(x) >= 1);
+        int n = 1;
+        int pow = -1;
+        double eps = 0.0001, s = 0, t = x;
+        while (Math.abs(t) > eps) {
+            t *= 1.0 * pow * (x/(n/(n-1)));
+            pow = -pow;
+            s += t ;
             n++;
         }
         System.out.print(s);
