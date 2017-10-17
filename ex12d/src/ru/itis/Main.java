829178a7961ed607c94a957ddf20bcf12be56c76
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-17 21:10:28 +0300

    исправленная ex12d 17.10

--- ex12d/src/ru/itis/Main.java
+++ ex12d/src/ru/itis/Main.java
@@ -7,13 +7,12 @@ public class Main {
         double x=in.nextInt();
         int n=1;
         int pow=-1;
-        double eps=0.0001,t=(x*x)/2,s;
-        s=1-t;
+        double eps=0.0001,t=1,s=0;
         while(Math.abs(t)>eps){
+            s+=t;
             t*=((1.0*pow*x*x)/(2*n));
             n++;
             pow=-pow;
-            s+=t;
         }
         System.out.print(s);
     }
