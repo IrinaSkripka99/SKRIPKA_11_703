81473e5275f424327425b9de8633542c80779a4e
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-30 22:45:09 +0300

    массивы дз

---
+++ HomeWork 1/src/ru/itis/Ex1.java
@@ -0,0 +1,30 @@
+package ru.itis;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        int m = a[0];
+        int col = 1;
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            a[i] = r.nextInt(20) - 5;
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < m) {
+                m = a[i];
+            }
+            if (a[i] == m) {
+                col++;
+            }
+        }
+        System.out.print(col);
+    }
+}
+
