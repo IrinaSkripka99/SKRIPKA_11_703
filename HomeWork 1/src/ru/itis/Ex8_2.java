0f715813bce7e9932c809723ea064e36efb85812
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-10-31 20:17:33 +0300

    массивы дз

---
+++ HomeWork 1/src/ru/itis/Ex8_2.java
@@ -0,0 +1,53 @@
+package ru.itis;
+import java.util.Random;
+
+public class Ex8_2 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[] b1 = new int[5];
+        int[] b2 = new int[5];
+
+        int[] s = {1, 2, 3, 4, 5};
+        int[] T1 = {0, 1, 2, 8};
+
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < T1.length; i++) {
+            System.out.print(T1[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < s.length; i++) {
+            if (exists(T1, s[i])) {
+                b1[i] = 1;
+            } else {
+                b1[i] = 0;
+            }
+            System.out.print(b1[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < b1.length; i++) {
+            b2[i] = r.nextInt(2);
+            System.out.print(b2[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            if (b2[i] == b1[i] && b2[i] == 1) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static boolean exists(int[] T, int x) {
+        for (int i = 0; i < T.length; i++) {
+            if (T[i] == x) return true;
+        }
+        return false;
+    }
+}
