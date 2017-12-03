0a26509e49ee57dfb55ce5d00fcb9c044d9d9531
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-12-03 15:57:40 +0300

    долги

---
+++ LESSONWORK/src/Work2911/Ex7.java
@@ -0,0 +1,9 @@
+package Work2911;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        String s1="Оля";
+        String s2="Ирина";
+        System.out.println(s1==s2);
+    }
+}
