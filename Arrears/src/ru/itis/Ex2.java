36872821aaddc4e7fc82aa966dc5534f6f1777d5
IrinaSkripka99 <irinaskripka99@gmail.com>
2017-12-19 18:03:34 +0300

    долги

--- Arrears/src/ru/itis/Ex2.java
+++ Arrears/src/ru/itis/Ex2.java
@@ -4,21 +4,27 @@ package ru.itis;
 public class Ex2 {
     public static void main(String[] args) {
         int[][] a = {{1, 2},
-                     {3, 4}};
+                {3, 4}};
         int[][] b = {{4, 3},
-                     {2, 1}};
-        System.out.print(proizvedenie(a,b));
+                {2, 1}};
+ proizvedenie(a, b);
     }
-    public static int[][] proizvedenie(int[][] a, int[][] b){
-        int[][]res=new int[a.length][b.length];
-        for (int i = 0; i <a.length ; i++) {
-            for (int j = 0; j <b[i].length ; j++) {
-                res[i][j]=0;
-                for (int k = 0; k <a.length; k++) {
+
+    public static void  proizvedenie(int[][] a, int[][] b) {
+        int[][] res = new int[a.length][b.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                res[i][j] = 0;
+                for (int k = 0; k < a.length; k++) {
                     res[i][j] += a[i][k] * a[k][j];
                 }
             }
         }
-        return res;
+        for (int i = 0; i <a.length ; i++) {
+            for (int j = 0; j <b.length ; j++) {
+                System.out.print(res[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
